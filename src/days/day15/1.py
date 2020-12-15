ac662ed52f166a223e06917783508073dce9b91c
skyying <dreamuk@gmail.com>
2020-12-15 23:59:08 +0800

    add day 15 part 2
    need to refactor

--- src/days/day15/1.py
+++ src/days/day15/1.py
@@ -11,40 +11,47 @@ def exec_part_1(n):
     spoken_in_turns = defaultdict(list)
     spoken_count=defaultdict(int)
     most_recent_spoken=None
+    ll = len(starting_numbers)
     while turn < n:
+        print(turn)
         turn += 1
         # starting number first
-        if i < len(starting_numbers):
+        if i < ll:
             most_recent_spoken = starting_numbers[i]
-            record_spoken(most_recent_spoken, spoken_count, spoken_in_turns, turn)
+            record_recent_spoken(spoken_in_turns, most_recent_spoken, turn)
             i+=1
             continue
-        elif spoken_count[most_recent_spoken] == 1:
+        elif len(spoken_in_turns[most_recent_spoken]) == 1:
             most_recent_spoken = 0
-            record_spoken(most_recent_spoken, spoken_count, spoken_in_turns, turn)
+            record_recent_spoken(spoken_in_turns, most_recent_spoken, turn)
 
-        elif spoken_count[most_recent_spoken] > 1:
+        elif len(spoken_in_turns[most_recent_spoken]) > 1:
             most_recent_spoken = calc_recent(spoken_in_turns[most_recent_spoken])
-            record_spoken(most_recent_spoken, spoken_count, spoken_in_turns, turn)
+            record_recent_spoken(spoken_in_turns, most_recent_spoken, turn)
 
-        print(most_recent_spoken)
-
-
-def record_spoken(most_recent_spoken, spoken_count, spoken_in_turns, turn):
-    record_recent_spoken(spoken_in_turns, most_recent_spoken, turn)
-    increase_spoken_count(most_recent_spoken, spoken_count)
+    print(most_recent_spoken)
 
 
-def increase_spoken_count(most_recent_spoken, spoken_count):
-    spoken_count[most_recent_spoken] += 1
+# def record_spoken(most_recent_spoken, spoken_in_turns, turn):
+    # increase_spoken_count(most_recent_spoken, spoken_count)
 
+
+# def increase_spoken_count(most_recent_spoken, spoken_count):
+#     spoken_count[most_recent_spoken] += 1
+#
 
 def record_recent_spoken(spoken_in_turns, most_recent_spoken, turn):
-    spoken_in_turns[most_recent_spoken].append(turn)
-    while len(spoken_in_turns[most_recent_spoken]) > 2:
-        spoken_in_turns[most_recent_spoken].pop(0)
+    if most_recent_spoken in spoken_in_turns:
+        spoken_in_turns[most_recent_spoken] = [spoken_in_turns[most_recent_spoken][-1], turn]
+    else:
+        spoken_in_turns[most_recent_spoken] = [turn]
     return spoken_in_turns
 
 
-exec_part_1(2020)
+print(exec_part_1(2020))
+
+
+# exec part 2 take a long time...
+# need to find a more efficient way
+print(exec_part_1(30000000))
 
