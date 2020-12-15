98f961b8349aa26d919d5e2f362256fb9520ce36
skyying <dreamuk@gmail.com>
2020-12-15 21:17:49 +0800

    refactor day 8 python version and add day8 bash version (part1)

--- src/days/day8/1.py
+++ src/days/day8/1.py
@@ -4,7 +4,6 @@ def read_instructions():
     f.close()
     return content
 
-
 def normalize_inst(inst):
     a, b = inst.split( )
     return a, int(b)
@@ -18,38 +17,41 @@ def perform_opt(inst, cur, acc):
     }[opt](cur, acc)
 
 def run_programming(instructions):
-    cur, accumulator, seen= 0, 0, set()
-    while cur not in seen and cur < len(instructions):
-        seen.add(cur)
-        cur, accumulator = perform_opt(instructions[cur], cur, accumulator)
-    return accumulator, cur == len(instructions)
-
-inst = read_instructions()
-
-swap_opts={
-    'jmp': 'nop',
-    'nop': 'jmp',
-}
+    cur_idx, accumulator, seen= 0, 0, set()
+    while cur_idx not in seen and cur_idx < len(instructions):
+        seen.add(cur_idx)
+        cur_idx, accumulator = perform_opt(instructions[cur_idx], cur_idx, accumulator)
+    return accumulator, cur_idx == len(instructions)
 
 def get_swap_opt(opt):
-    return swap_opts[opt]
+    return {
+        'jmp': 'nop',
+        'nop': 'jmp',
+        'acc': 'acc',
+    }[opt]
 
-def replaced(opt, instruction):
+def swapped_opt(opt, instruction):
     return instruction.replace(opt, get_swap_opt(opt))
 
-def exec_part2():
-    instructions = read_instructions()
-    print(instructions)
-    n=len(instructions)
-    for i in range(0, n):
-        opt, _ = normalize_inst(instructions[i])
-        if opt not in swap_opts:
-            cur = instructions[i]
-            instructions[i] = replaced(opt, cur)
-            acc, correct = run_programming(instructions)
-            if correct:
-                return acc
-            else:
-                instructions[i] = cur
+def exec_part1(instructions):
+    acc, correct = run_programming(instructions)
+    return acc
 
-exec_part2()
+def exec_part2(instructions):
+    n=len(instructions)
+    for i in range(n):
+        opt, _ = normalize_inst(instructions[i])
+        current_instruction = instructions[i]
+        instructions[i] = swapped_opt(opt, current_instruction)
+        acc, correct = run_programming(instructions)
+        if correct:
+            return acc
+        else:
+            instructions[i] = current_instruction
+    return None
+
+
+instruction_list=read_instructions()
+
+print("Part 1:", exec_part1(instruction_list))
+print("Part 2:", exec_part2(instruction_list))
---
+++ src/days/day8/1.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+instructions=()
+
+while IFS= read -a line; do
+  instructions+=("$line")
+done < ./in
+
+ary_len=${#instructions[@]}
+declare -A seen;
+
+function part_one () {
+  local instructions=("$@")
+  i=0
+  acc=0
+  while (( i < ary_len )); do
+    (( seen[$i] > 0 )) && break
+    seen["$i"]="$i"
+    IFS=" " read -a rules <<< "${instructions["$i"]}"
+    opt=${rules[0]}
+    val=${rules[1]}
+    if [[ $opt == 'acc' ]]; then
+      acc=$(( acc + val))
+      (( i++ ))
+    elif [[ $opt == 'jmp' ]]; then
+      i=$(( i + val ))
+    elif [[ $opt == 'nop' ]]; then
+      (( i++ ))
+    else
+      pass
+    fi
+  done
+  printf "Part 1: %s\n" "$acc"
+}
+
+function Part_2() {
+
+}
+
+
+part_one "${instructions[@]}"
+
